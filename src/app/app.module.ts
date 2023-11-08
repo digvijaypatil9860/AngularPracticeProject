a076755485154e0f55a507c5d7c0413ddca0874a
Digvijay Patil <digvijaypatil9860@gmail.com>
2023-11-08 07:55:48 +0530

    all files added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { ImagesComponent } from './images/images.component';
+import { ChoreListComponent } from './chore-list/chore-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MovieListComponent,
+    ImagesComponent,
+    ChoreListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
